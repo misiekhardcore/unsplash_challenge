0d772befc0ab0c6c3eb343eff9a402eaaebc1790
misiekhardcore <misiekhardcore@gmail.com>
2021-06-29 22:04:10 +0200

    Rename Component

--- frontend/src/Components/Input.tsx
+++ frontend/src/Components/Input.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const SearchInput = styled.input`
+export const Input = styled.input`
   padding: 1.5rem;
   border: 1px solid #bdbdbd;
   filter: drop-shadow(0px 1px 6px rgba(0, 0, 0, 0.1));
